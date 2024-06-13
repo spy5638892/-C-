47fe62c6da5a128503bcd3528b106918d0cfda75
spy5638892 <145102554+spy5638892@users.noreply.github.com>
2024-06-13 11:54:10 +0800

    Add files via upload

---
+++ 测试.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int add(int a , int b){
+    return a+b;
+}
+#define Jks 300
+
+
+
+
+int main()
+{
+    int a, b,c,d;
+    a=1;
+    a=++a;//2
+    b=a++;//b=2,a=3
+    printf("%d\n",a);
+    c=(a++ -(b++));//c=1,a=4,b=3;
+    d=++c;//d=2,c=2
+// 4,3,c2,2
+        printf("%d,%d,%d,%d", a,b,c,d);
+        
+    return 0;
+}
