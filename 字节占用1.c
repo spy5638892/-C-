4f05e3715da631e49459bc0bc8e9d1defa594028
spy5638892 <145102554+spy5638892@users.noreply.github.com>
2024-06-18 18:08:47 +0800

    Update and rename 函数.c to 字节占用1.c

---
+++ 字节占用1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char a;
+    printf("sizeof = nihao = %d\n", sizeof("nihao"));
+    printf("sizeof = a = %d\n", sizeof('a'));
+    return 0;
+}
