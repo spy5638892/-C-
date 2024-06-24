544390f4f991b5f962ea6be9e6790e3347187971
spy5638892 <145102554+spy5638892@users.noreply.github.com>
2024-06-24 18:39:54 +0800

    Add files via upload

---
+++ 函数.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{  
+    int mun=0 ,i=1;
+    while(i<=100){
+        mun=mun+i;
+        i++;
+    }
+    printf("%d",mun);
+   
+    return 0;
+}
